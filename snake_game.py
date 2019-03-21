013f7d8abf619ffa880d8f4a0a6952e84197818b
Grant Merrill <g-mips@users.noreply.github.com>
2019-03-20 22:43:29 -0600

    Added FPS and playtime elements. Got blitting to work.

--- snake_game.py
+++ snake_game.py
@@ -2,11 +2,8 @@ import pygame
 
 def setup_initial_screen(width, height):
     screen = pygame.display.set_mode((width, height))
-    background = pygame.Surface(screen.get_size())
-    background.fill((255,255,255))
-    background = background.convert()
 
-    return screen, background
+    return screen
 
 def event_handler():
     '''
@@ -21,16 +18,38 @@ def event_handler():
 
     return True
 
+def create_surface(size, red, green, blue):
+    surface = pygame.Surface(size)
+    surface.fill((red, green, blue))
+    surface = surface.convert()
+    return surface
+
 def main():
     pygame.init()
-    screen, background = setup_initial_screen(640, 480)
+    clock = pygame.time.Clock()
+
+    width = 640
+    height = 480
+
+    screen = setup_initial_screen(width, height)
+
+    background = create_surface(screen.get_size(), 255, 0, 0)
+    foreground = create_surface(screen.get_size(), 0, 255, 0)
+    screen.blit(background, (0, 0))
+    screen.blit(foreground, (0, height/2))
 
     running = True
+    FPS = 60
+    playtime = 0.0
     while running:
+        milliseconds = clock.tick(FPS)
+        playtime += milliseconds / 1000.0
         if not event_handler():
             running = False
+
+        text = "FPS: {0:.2f} Playtime: {1:.2f}".format(clock.get_fps(), playtime)
+        pygame.display.set_caption(text)
         pygame.display.flip()
-        screen.blit(background, (0, 0))
 
     pygame.quit()
 
