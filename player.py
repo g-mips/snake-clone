53a3bea8c1077863be7ac028566f3fac8a3b4757
g-mips <g-mips@users.noreply.github.com>
2019-06-26 18:13:06 -0700

    Added grid for debugging. Moved things to a level surface. Adjusted collision detecting. Finished logic of game. (needs touch up)

--- player.py
+++ player.py
@@ -3,13 +3,19 @@ import copy
 
 from colors import *
 
-snake_part_size = 20
+snake_head_size = 20
+snake_part_size = 19
 
 class SnakeHead():
-    def __init__(self, background, x=50, y=50):
+    def __init__(self, background, x=60, y=60):
+        self.moves = []
         self.cur_dir = pygame.K_RIGHT
         self.snake_part = pygame.draw.rect(background, BLACK,
-            (x, y, snake_part_size, snake_part_size), 2)
+            (x, y, snake_head_size, snake_head_size), 2)
+
+def update_snake_head_moves(snake_head, key):
+    snake_head.cur_dir = key
+    snake_head.moves.append(key)
 
 def check_snake_events(snake_head, event):
     '''
@@ -18,39 +24,49 @@ def check_snake_events(snake_head, event):
     if event.type == pygame.KEYDOWN:
         if snake_head.cur_dir != pygame.K_DOWN and \
             event.key == pygame.K_UP:
-            snake_head.cur_dir = event.key
+            update_snake_head_moves(snake_head, event.key)
         elif snake_head.cur_dir != pygame.K_UP and \
             event.key == pygame.K_DOWN:
-            snake_head.cur_dir = event.key
+            update_snake_head_moves(snake_head, event.key)
         elif snake_head.cur_dir != pygame.K_LEFT and \
             event.key == pygame.K_RIGHT:
-            snake_head.cur_dir = event.key
+            update_snake_head_moves(snake_head, event.key)
         elif snake_head.cur_dir != pygame.K_RIGHT and \
             event.key == pygame.K_LEFT:
-            snake_head.cur_dir = event.key
+            update_snake_head_moves(snake_head, event.key)
+
+def move_snake_head(snake_head, direction):
+    if direction == pygame.K_LEFT:
+        snake_head.snake_part.move_ip(-snake_head_size, 0)
+    elif direction == pygame.K_RIGHT:
+        snake_head.snake_part.move_ip(snake_head_size, 0)
+    elif direction == pygame.K_DOWN:
+        snake_head.snake_part.move_ip(0, snake_head_size)
+    elif direction == pygame.K_UP:
+        snake_head.snake_part.move_ip(0, -snake_head_size)
 
 def update_snake_head(snake_head, background):
     '''
     Update the state of the snake
     '''
-    if snake_head.cur_dir == pygame.K_LEFT:
-        snake_head.snake_part.move_ip(-(snake_part_size + 1), 0)
-    elif snake_head.cur_dir == pygame.K_RIGHT:
-        snake_head.snake_part.move_ip(snake_part_size + 1, 0)
-    elif snake_head.cur_dir == pygame.K_DOWN:
-        snake_head.snake_part.move_ip(0, snake_part_size + 1)
-    elif snake_head.cur_dir == pygame.K_UP:
-        snake_head.snake_part.move_ip(0, -(snake_part_size + 1))
+    # Just move the snake in the current direction if there are no moves
+    # Otherwise excute the first move on the list and remove it
+    if len(snake_head.moves) == 0:
+        move_snake_head(snake_head, snake_head.cur_dir)
+    else:
+        move_snake_head(snake_head, snake_head.moves[0])
+        snake_head.moves.remove(snake_head.moves[0])
 
+    # Draw the position
     pygame.draw.rect(background, BLACK,
         snake_head.snake_part, 2)
 
 class SnakeBody():
-    def __init__(self, background, x, y):
-        self.on_screen = False
+    def __init__(self, background, x, y, on_screen=False, next_part=None):
+        self.on_screen = on_screen
         self.snake_part = pygame.draw.rect(background, BLACK,
             (x, y, snake_part_size, snake_part_size), 2)
-        self.next_part = None
+        self.next_part = next_part
 
 def update_snake_body(cur_body, prev_snake_part, background):
     '''
--- snake_game.py
+++ snake_game.py
@@ -7,7 +7,22 @@ import copy
 from colors import *
 import player
 
+prize_size = player.snake_part_size
+
 border_dist = 5
+debug = False
+
+def show_grid(background):
+    '''
+    Show a grid based on the snake head size within the border of the
+    background
+    '''
+    width, height = background.get_size()
+
+    for x in range(0, width, player.snake_head_size):
+        for y in range(0, height, player.snake_head_size):
+            pygame.draw.line(background, GREEN, (0, y), (width, y), 1)
+        pygame.draw.line(background, GREEN, (x, 0), (x, height), 1)
 
 def setup_borders(background):
     '''
@@ -17,15 +32,15 @@ def setup_borders(background):
     '''
     width, height = background.get_size()
 
-    line_1_start = (border_dist, border_dist)
-    line_2_start = (border_dist, height - border_dist)
-    line_3_start = (width - border_dist, height - border_dist)
-    line_4_start = (width - border_dist, border_dist)
+    line_1_start = (0, 0)
+    line_2_start = (0, height)
+    line_3_start = (width, height)
+    line_4_start = (width, 0)
 
-    return [pygame.draw.line(background, BLACK, line_1_start, line_2_start, 5),
-        pygame.draw.line(background, BLACK, line_2_start, line_3_start, 5),
-        pygame.draw.line(background, BLACK, line_3_start, line_4_start, 5),
-        pygame.draw.line(background, BLACK, line_4_start, line_1_start, 5)]
+    pygame.draw.line(background, BLACK, line_1_start, line_2_start, 5)
+    pygame.draw.line(background, BLACK, line_2_start, line_3_start, 5)
+    pygame.draw.line(background, BLACK, line_3_start, line_4_start, 5)
+    pygame.draw.line(background, BLACK, line_4_start, line_1_start, 5)
 
 def create_random_prize(background):
     '''
@@ -33,15 +48,13 @@ def create_random_prize(background):
     '''
     width, height = background.get_size()
 
-    prize_width = 20
-    prize_height = 20
-    x = random.randrange(border_dist + 1,
-        width - (border_dist + 1) - prize_width)
-    y = random.randrange(border_dist + 1,
-        height - (border_dist + 1) - prize_width)
+    x = random.randrange(player.snake_head_size + 1,
+        width - player.snake_head_size - 1, player.snake_head_size)
+    y = random.randrange(player.snake_head_size + 1,
+        height - player.snake_head_size - 1, player.snake_head_size)
 
     return pygame.draw.rect(background, RED,
-        (x, y, prize_width, prize_height), 2)
+        (x, y, prize_size, prize_size), 1)
 
 def event_handler(snake_head):
     '''
@@ -58,12 +71,12 @@ def event_handler(snake_head):
 
     return True
 
-def create_surface(size, red, green, blue):
+def create_surface(size, color):
     '''
     Create a surface in an initial state
     '''
     surface = pygame.Surface(size)
-    surface.fill((red, green, blue))
+    surface.fill(color)
 
     return surface.convert()
 
@@ -74,55 +87,86 @@ def main(width, height):
     clock = pygame.time.Clock()
     FPS = 10
 
-    # Setup screen and objects
+    # Setup screens
     main_screen = pygame.display.set_mode((width, height))
-    snake_head = player.SnakeHead(main_screen)
-    borders = setup_borders(main_screen)
-    prize = create_random_prize(main_screen)
+    level_width = width - (border_dist * 2) - 2
+    level_height = height - (border_dist * 2) - 2
+    level = create_surface((level_width, level_height), WHITE)
+
+    # Setup snake
+    snake_head = player.SnakeHead(level)
+    snake_head_part = pygame.Rect(
+        snake_head.snake_part.x + 1, snake_head.snake_part.y + 1,
+        player.snake_part_size, player.snake_part_size)
+    snake_parts = [ player.SnakeBody(level,
+        snake_head_part.x - player.snake_head_size - 1, 60,
+        True, snake_head_part) ]
+    snake_parts.append(player.SnakeBody(level,
+        snake_head_part.x - (player.snake_head_size * 2) - 1, 60,
+        True, snake_parts[0].snake_part))
+
+    # Setup Prize
+    prize = create_random_prize(level)
+
+    # Setup Border
+    setup_borders(level)
+
+    # Display level on main screen
+    main_screen.blit(level, (border_dist + 2, border_dist + 2))
 
     # Main loop
     running = True
     playtime = 0.0
-    snake_parts = []
+
     while running:
         milliseconds = clock.tick(FPS)
-        playtime += milliseconds / 1000.0
 
         # Debug info
-        text = "FPS: {0:.2f} Playtime: {1:.2f}".format(
-            clock.get_fps(), playtime)
-        pygame.display.set_caption(text)
+        if debug:
+            playtime += milliseconds / 1000.0
+            text = "FPS: {0:.2f} Playtime: {1:.2f}".format(
+                    clock.get_fps(), playtime)
+            pygame.display.set_caption(text)
 
         # Run through all the events
         running = event_handler(snake_head)
-
         if running:
             # Clear the screen
             main_screen.fill(WHITE)
+            level.fill(WHITE)
+
+            if debug:
+                show_grid(level)
 
             # Update the snake head
-            player.update_snake_head(snake_head, main_screen)
+            player.update_snake_head(snake_head, level)
 
             # Update the snake body
-            prev_snake = snake_head.snake_part
+            prev_snake = pygame.Rect(
+                    snake_head.snake_part.x + 1, snake_head.snake_part.y + 1,
+                    player.snake_part_size, player.snake_part_size)
             for snake_part in snake_parts:
-                player.update_snake_body(snake_part, prev_snake, main_screen)
+                player.update_snake_body(snake_part, prev_snake, level)
                 prev_snake = snake_part.snake_part
 
-            # Draw the borders
-            borders = setup_borders(main_screen)
-
             # Draw the prize
-            pygame.draw.rect(main_screen, RED, prize, 2)
+            pygame.draw.rect(level, RED, prize, 2)
 
-            # Update the screen
+            # Draw the borders
+            setup_borders(level)
+
+            # Update the screens
+            main_screen.blit(level, (border_dist + 2, border_dist + 2))
             pygame.display.update()
 
             # Test to see if the snake ran into a border
-            for border in borders:
-                if snake_head.snake_part.colliderect(border):
-                    running = False
-                    break
+            if snake_head.snake_part.x < 0 or \
+                (snake_head.snake_part.x +
+                 snake_head.snake_part.width) > level_width or \
+                snake_head.snake_part.y < 0 or \
+                (snake_head.snake_part.y +
+                 snake_head.snake_part.height) > level_height:
+                running = False
 
             # Test to see if the snake head ran into the snake body
             if running:
@@ -130,18 +174,28 @@ def main(width, height):
                     if snake_part.on_screen and \
                         snake_head.snake_part.colliderect(
                             snake_part.snake_part):
+                        print('snake part')
                         running = False
                         break
 
             # Test to see if the snake head collected a prize
             if running and snake_head.snake_part.colliderect(prize):
-                prize = create_random_prize(main_screen)
+                prize = create_random_prize(level)
                 x = snake_head.snake_part.x
                 y = snake_head.snake_part.y
 
                 if len(snake_parts) > 0:
                     x = snake_parts[len(snake_parts) - 1].snake_part.x
                     y = snake_parts[len(snake_parts) - 1].snake_part.y
+                else:
+                    if snake_head.cur_dir == pygame.K_LEFT:
+                        x += 1
+                    elif snake_head.cur_dir == pygame.K_RIGHT:
+                        x -= 1
+                    elif snake_head.cur_dir == pygame.K_DOWN:
+                        y -= 1
+                    elif snake_head.cur_dir == pygame.K_UP:
+                        y += 1
 
                 snake_parts.append(player.SnakeBody(main_screen, x, y))
 
@@ -151,13 +205,13 @@ def usage():
     print('{} [-W <width>] [-H <height>]'.format(sys.argv[0]))
 
 if __name__ == "__main__":
-    width = 640
-    height = 480
+    width = 655
+    height = 495
 
     if len(sys.argv) != 1:
         try:
-            opts, args = getopt.getopt(sys.argv[1:], "hW:H:",
-                ["help", "width=", "height="])
+            opts, args = getopt.getopt(sys.argv[1:], "hdW:H:",
+                ["help", "debug", "width=", "height="])
         except getopt.GetoptError:
             usage()
             sys.exit(1)
@@ -166,6 +220,8 @@ if __name__ == "__main__":
             if opt in ('-h', '--help'):
                 usage()
                 sys.exit()
+            elif opt in ('-d', '--debug'):
+                debug = True
             elif opt in ('-W', '--width'):
                 width = arg
             elif opt in ('-H', '--height'):
